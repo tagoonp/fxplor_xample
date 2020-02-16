d7418f23f6207b58ac7c1f34727f3d74e0a9d9ad
Tagoon Prappre <tagoon.p@psu.ac.th>
2020-02-16 21:48:38 +0700

    20200216 21.29

--- script2.r
+++ script2.r
@@ -1,16 +1,13 @@
 args <- commandArgs(TRUE)
 
 N <- args[1]
+Prefix <- args[2]
 #N <- 100
 x <- rnorm(N,0,1)
 
 # Writing mtcars data
 rnd_url <- "/var/www/html/fxplor_xample/tmp_result/"
-myFun <- function(n = 5000) {
-  a <- do.call(paste0, replicate(5, sample(LETTERS, n, TRUE), FALSE))
-  paste0(a, sprintf("%04d", sample(9999, n, TRUE)), sample(LETTERS, n, TRUE))
-}
-rnd_string <- myFun(1)
+rnd_string <- Prefix
 static_fname <- "_temp.png"
 
 result_filefullname <- paste(rnd_url, rnd_string, static_fname,sep="")
--- test_script2.php
+++ test_script2.php
@@ -2,13 +2,24 @@
 if(isset($_GET['N']))
 {
   $N = $_GET['N'];
-
+  $prefix = generateRandomString(5);
   // execute R script from shell
   // this will save a plot at temp.png to the filesystem
-  exec("Rscript script2.r $N");
+  exec("Rscript script2.r $N $prefix");
 
   // return image tag
   $nocache = rand();
-  echo("");
+  echo('<a href="./tmp_result/'.$prefix.'_temp.png" target="_blank">Link</a>');
+
+}
+
+function generateRandomString($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
 }
 ?>
